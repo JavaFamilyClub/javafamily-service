5411d36638d6c467d1914e364c505d8aec024209
李帅 <lishuai@mlogcn.com>
2022-07-11 18:52:11 +0800

    add swagger2word

---
+++ javafamily-swagger2word/src/main/java/club/javafamily/Application9500.java
@@ -0,0 +1,16 @@
+package club.javafamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jack Li
+ * @date 2022/7/11 下午6:29
+ * @description Swagger 转化 Word 工具
+ */
+@SpringBootApplication
+public class Application9500 {
+   public static void main(String[] args) {
+      SpringApplication.run(Application9500.class, args);
+   }
+}
