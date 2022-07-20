98663c5009603a5ea67e4bb39cd0df9403da7aaf
sunnyhe <sunnyhe@inetsoft.com>
2022-07-20 22:08:00 +0800

    1. 支持 post 及 card 通知 2. 支持title, content 模板输入.

---
+++ prometheus-webhook-feishu/src/main/java/club/javafamily/prom/constant/SystemConstant.java
@@ -0,0 +1,39 @@
+package club.javafamily.prom.constant;
+
+public interface SystemConstant {
+
+    /**
+     * alert manager 推送过来包含 annotation 的 key
+     */
+    String ANNO_KEY = "commonAnnotations";
+
+    /**
+     * title 模板 key
+     */
+    String TITLE_TEMPLATE = "titleTemplate";
+
+    /**
+     * 内容模板 key
+     */
+    String CONTENT_TEMPLATE = "template";
+
+    /**
+     * title 直接取值的 key
+     */
+    String TITLE_KEY = "title";
+
+    /**
+     * content 直接取值的 key
+     */
+    String CONTENT_KEY = "content";
+
+    /**
+     * btn 直接取值的 key
+     */
+    String BTN_LABEL_KEY = "btn";
+
+    /**
+     * button link 直接取值的 key
+     */
+    String BTN_LINK_KEY = "link";
+}
--- prometheus-webhook-feishu/src/main/java/club/javafamily/prom/controller/AlertController.java
+++ prometheus-webhook-feishu/src/main/java/club/javafamily/prom/controller/AlertController.java
@@ -1,13 +1,19 @@
 package club.javafamily.prom.controller;
 
+import club.javafamily.nf.request.FeiShuCardNotifyRequest;
 import club.javafamily.nf.request.FeiShuPostNotifyRequest;
 import club.javafamily.nf.request.FeiShuTextNotifyRequest;
 import club.javafamily.nf.request.tags.BaseTextTagContentItem;
 import club.javafamily.nf.request.tags.LinkTagContentItem;
 import club.javafamily.nf.service.FeiShuNotifyHandler;
+import club.javafamily.prom.constant.SystemConstant;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.text.StringSubstitutor;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @author Jack Li
@@ -28,10 +34,11 @@ public class AlertController {
    public String alertText(@RequestBody String body) {
       final JSONObject json = JSONObject.parseObject(body);
 
-      final String text = json.getJSONObject("commonAnnotations")
-         .getString("description");
+      JSONObject jsonObject = json.getJSONObject(SystemConstant.ANNO_KEY);
 
-      log.info("Request received, body is: \n{}\n, text is {} \n",
+      String text = parseContent(jsonObject, SystemConstant.CONTENT_TEMPLATE, SystemConstant.CONTENT_KEY);
+
+      log.info("Request received, body is: \n\n{}\n\n, text is {} \n",
          json.toJSONString(), text);
 
       final FeiShuTextNotifyRequest request
@@ -39,23 +46,81 @@ public class AlertController {
 
       final String response = feiShuNotifyHandler.notify(request);
 
-      log.info("\nRequestResponse is : {}\n", request);
+      log.info("\n alertText response is : {}\n", request);
 
       return response;
    }
 
    @PostMapping("/alert/post")
-   public String alertPost(@RequestParam("title") String title,
-                           @RequestParam("content") String content,
-                           @RequestParam("btnLabel") String btnLabel,
-                           @RequestParam("btnLink") String btnLink)
-   {
+   public String alertPost(@RequestBody String body) {
+      final JSONObject json = JSONObject.parseObject(body);
+
+      JSONObject jsonObject = json.getJSONObject(SystemConstant.ANNO_KEY);
+
+      String title = parseContent(jsonObject, SystemConstant.TITLE_TEMPLATE, SystemConstant.TITLE_KEY);
+      String content = parseContent(jsonObject, SystemConstant.CONTENT_TEMPLATE, SystemConstant.CONTENT_KEY);
+      String btnLabel = parseContent(jsonObject, null, SystemConstant.BTN_LABEL_KEY);
+      String btnLink = parseContent(jsonObject, null, SystemConstant.BTN_LINK_KEY);
+
+      log.info("Request received, body is: \n\n{}\n\n, title is {}, content is {}, label is {}, link is {} \n",
+              json.toJSONString(), title, content, btnLabel, btnLink);
+
       final FeiShuPostNotifyRequest request = FeiShuPostNotifyRequest.of(
          title,
          new BaseTextTagContentItem<>(content),
          new LinkTagContentItem(btnLabel, btnLink));
 
-      return feiShuNotifyHandler.notify(request);
+      String response = feiShuNotifyHandler.notify(request);
+
+      log.info("\n alertPost response is : {}\n", request);
+
+      return response;
+   }
+
+   @PostMapping("/alert/card")
+   public String alertCard(@RequestBody String body) {
+      final JSONObject json = JSONObject.parseObject(body);
+
+      JSONObject jsonObject = json.getJSONObject(SystemConstant.ANNO_KEY);
+
+      String title = parseContent(jsonObject, SystemConstant.TITLE_TEMPLATE, SystemConstant.TITLE_KEY);
+      String content = parseContent(jsonObject, SystemConstant.CONTENT_TEMPLATE, SystemConstant.CONTENT_KEY);
+      String btnLabel = parseContent(jsonObject, null, SystemConstant.BTN_LABEL_KEY);
+      String btnLink = parseContent(jsonObject, null, SystemConstant.BTN_LINK_KEY);
+
+      log.info("Request received, body is: \n\n{}\n\n, title is {}, content is {}, label is {}, link is {} \n",
+              json.toJSONString(), title, content, btnLabel, btnLink);
+
+      final FeiShuCardNotifyRequest request = FeiShuCardNotifyRequest.of(
+              title, content, btnLabel, btnLink);
+
+      String response = feiShuNotifyHandler.notify(request);
+
+      log.info("\n alertCard response is : {}\n", request);
+
+      return response;
+   }
+
+   /**
+    * 格式化内容
+    * @param jsonObject commonAnnotations
+    * @param templateKey 模板 key
+    * @param valueKey 值key
+    * @return 格式化后的内容
+    */
+   private String parseContent(JSONObject jsonObject,
+                               String templateKey,
+                               String valueKey)
+   {
+      if(ObjectUtils.isEmpty(templateKey)) {
+         return jsonObject.getString(valueKey);
+      }
+
+      String template = jsonObject.getString(templateKey);
+
+      Map<String, Object> params = jsonObject.getInnerMap();
+
+      return StringSubstitutor.replace(template, params);
    }
 
 }
