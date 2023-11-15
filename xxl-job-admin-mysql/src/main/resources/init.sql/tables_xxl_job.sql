a707343bd02edc7f6fef70a8327cbf5b7e7eee77
javafamily <javafamily.club@outlook.com>
2023-11-15 16:34:08 +0800

    feat: executor param support large string, max from 512 to 14000.

--- xxl-job-admin-mysql/src/main/resources/init.sql/tables_xxl_job.sql
+++ xxl-job-admin-mysql/src/main/resources/init.sql/tables_xxl_job.sql
@@ -38,7 +38,7 @@ CREATE TABLE if NOT EXISTS `xxl_job_log` (
   `job_id` int(11) NOT NULL COMMENT '任务，主键ID',
   `executor_address` varchar(255) DEFAULT NULL COMMENT '执行器地址，本次执行的地址',
   `executor_handler` varchar(255) DEFAULT NULL COMMENT '执行器任务handler',
-  `executor_param` varchar(512) DEFAULT NULL COMMENT '执行器任务参数',
+  `executor_param` varchar(14000) DEFAULT NULL COMMENT '执行器任务参数',
   `executor_sharding_param` varchar(20) DEFAULT NULL COMMENT '执行器任务分片参数，格式如 1/2',
   `executor_fail_retry_count` int(11) NOT NULL DEFAULT '0' COMMENT '失败重试次数',
   `trigger_time` datetime DEFAULT NULL COMMENT '调度-时间',
